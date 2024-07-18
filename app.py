cb0819ac79e0d6fe9f4aaba33b7c1578501518bc
Jovani Ortiz Barraza <yourgitemail@example.com>
2024-07-17 18:48:51 -0700

    se creo uina carpeta

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# nombre dde la variable = valor de la variable
+# esta variable es global
+estado_sistema = "apagado"
+consumo_luz = 0
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Invernadero inteligente!'
+
+
+@app.get("/humedad")
+def obtener_datos_generales():
+    if estado_sistema != "Encendido":
+        return jsonify(mensaje="Errror al obtener datos sistemas apagado"), 500
+
+    print("Obteniendo temperatura")
+    return jsonify(temperatura=34, humedad=22, viento="32km")
+
+
+@app.get("/apagado")
+def apagar_sistema():
+    global  estado_sistema
+    print("Apagando sistema")
+    estado_sistema = "apagado"
+    return jsonify(mensaje="sistema apagado con exito")
+
+
+@app.get("/encender")
+def encender_sistemas():
+    global estado_sistema
+    print("encender sistemas")
+    estado_sistema = "Encendido"
+    return jsonify(mensaje="sistema encendido con exito")
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
